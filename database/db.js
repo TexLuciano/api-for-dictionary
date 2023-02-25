56038afc93a72c11d3cec559926032552d09c46f
TexLuciano <lucianomartinello15@gmail.com>
2023-02-25 18:43:13 -0300

    att

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+const dbPass = process.env.DB_PASS;
 const dbUser = process.env.DB_USER;
 import mongoose from 'mongoose';
 
@@ -10,7 +10,7 @@ async function conectDb(){
  
   mongoose.set('strictQuery', true)
   await mongoose
-  .connect(`mongodb+srv://${dbUser}:${dbUser}@cluster0.4isr0ao.mongodb.net/meubanco?retryWrites=true&w=majority`)
+  .connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.4isr0ao.mongodb.net/meubanco?retryWrites=true&w=majority`)
  
 
 }
